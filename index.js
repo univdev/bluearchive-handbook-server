34ae0f7ed6b7e69be9e7e8e291a9d3ed1e16929d
pcy <pcydeveloper@gmail.com>
2021-12-17 17:59:19 +0900

    feat: 프로그램 시작부 예외처리

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ app.use(routes);
 const learnCommands = async () => {
   const questions = await commands();
   const entities = [...questions].map((q) => q.entities);
-  console.log(questions[0]);
   Nlp.learnAllData(questions);
   Nlp.setEntities(entities);
   await Nlp.train();
 }
 
 app.listen('8080', async () => {
-  console.log('키워드 학습중');
-  await learnCommands();
-  console.log('Start!');
+  try {
+    console.log('키워드 학습중');
+    await learnCommands();
+    console.log('Start!');
+  } catch (e) {
+    const { message } = e;
+    console.error(message);
+  }
 });
